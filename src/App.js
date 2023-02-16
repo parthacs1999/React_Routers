a00f4c932d729528733e7dbd400cf625a7eb898a
Partha Chakraborty <partha.c1999@gmail.com>
2023-02-16 22:00:23 +0530

    basic routing logic added using react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import {createBrowserRouter,RouterProvider,createRoutesFromElements,Route} from 'react-router-dom';
+import Home from './pages/Home';
+import Products from './pages/Products';
+const router=createBrowserRouter([
+  {path:'/',element:<Home/>},
+  {path:'/products',element:<Products/>}
+])
+// const routeDefinition=createRoutesFromElements(
+//   <Route>
+//     <Route path='/' element={<Home/>}/>
+//     <Route path='/products' element={<Products />} />
+//   </Route>
+// );
+// const router=createBrowserRouter(routeDefinition);
+
 function App() {
-  return <div></div>;
+  return (
+    <RouterProvider router={router}/>
+  )
 }
 
 export default App;
