b6f91b05839a274e8e05d20c2b105cdf098e9605
Partha Chakraborty <partha.c1999@gmail.com>
2023-02-16 22:22:56 +0530

    Navigating between pages with links ,layouts and nested routes logic added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import {createBrowserRouter,RouterProvider,createRoutesFromElements,Route} from 'react-router-dom';
 import Home from './pages/Home';
 import Products from './pages/Products';
+import Root from './pages/Root';
 const router=createBrowserRouter([
-  {path:'/',element:<Home/>},
-  {path:'/products',element:<Products/>}
+  {path:'/',element:<Root/>,children:[
+    { path: '/', element: <Home /> },
+    { path: '/products', element: <Products /> }
+  ]},
 ])
 // const routeDefinition=createRoutesFromElements(
 //   <Route>
